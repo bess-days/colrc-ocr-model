d7867a1da707592589919c43238e50c59653828a
Sydney Greenspun <grimsby8610@gmail.com>
2025-10-02 23:08:01 +0000

    model and venv

--- app/model.py
+++ app/model.py
@@ -1,4 +1,7 @@
 from sklearn.model_selection import train_test_split
+import torch
+from torch.utils.data import Dataset
+
 from app.generate import entries
 def make_dataset(test_size=0.2, random_state=42):
     train, val = train_test_split(entries, test_size=test_size, random_state=random_state)
