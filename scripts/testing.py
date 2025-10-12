7a28dfeb6b9214c560424d0724109cb68f437b4f
Sydney Greenspun <grimsby8610@gmail.com>
2025-10-12 14:57:15 -0700

    preparing to test

---
+++ scripts/testing.py
@@ -0,0 +1,15 @@
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+from PIL import Image
+import torch
+
+# Paths
+model_dir = "outputs/trocr-salish-en"
+processor = TrOCRProcessor.from_pretrained(model_dir)
+model = VisionEncoderDecoderModel.from_pretrained(model_dir)
+img = Image.open("test/screenshot.png").convert("RGB")
+
+pixel_values = processor(images=img, return_tensors="pt").pixel_values
+generated_ids = model.generate(pixel_values)
+pred_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+print("🔹 Predicted:", pred_text)
