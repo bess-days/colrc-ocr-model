30284e7a75dca95c571eef7443385f30f69b4a36
Sydney Greenspun <grimsby8610@gmail.com>
2025-10-12 22:31:27 +0000

    editing font path

--- scripts/1_generate.py
+++ scripts/1_generate.py
@@ -23,8 +23,8 @@ def make_text_sample(salish_words, english_words):
     return random.choice(layouts)
 os.makedirs("synthetic/images", exist_ok=True)
 os.makedirs("synthetic/labels", exist_ok=True)
-font_cfg = json.load(open("sources/font_config.json"))
-all_fonts = font_cfg["Charis", "Duolos",  "NotoSans"]
+font_cfg = json.load(open("sources/fonts_config.json"))
+all_fonts = font_cfg["Charis"] +font_cfg["Duolos"] +  font_cfg["NotoSans"]
 def random_font():
     fpath = random.choice(all_fonts)
     size = random.randint(24, 64)
